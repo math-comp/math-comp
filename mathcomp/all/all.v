dd8e1530e3da0d53a65657fee2da29b84b735e1a
Enrico Tassi <gares@fettunta.org>
2015-11-05 11:36:29 +0100

    removing files not in mathcomp-1.6

--- mathcomp/all/all.v
+++
@@ -1,6 +0,0 @@
-Require Export mathcomp.ssreflect.all_ssreflect.
-Require Export mathcomp.algebra.all_algebra.
-Require Export mathcomp.field.all_field.
-Require Export mathcomp.character.all_character.
-Require Export mathcomp.fingroup.all_fingroup.
-Require Export mathcomp.solvable.all_solvable.
--- mathcomp/fingroup/all_fingroup.v
+++
@@ -1,9 +0,0 @@
-Require Export action.
-Require Export automorphism.
-Require Export fingroup.
-Require Export gproduct.
-Require Export morphism.
-Require Export perm.
-Require Export presentation.
-Require Export quotient.
-
