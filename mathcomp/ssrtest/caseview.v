d54b8dff818f0b1218df14cfb2b813da93154fa9
Enrico Tassi <gares@fettunta.org>
2018-04-12 09:54:36 +0200

    remove ssrtest: it now belongs to Coq

--- mathcomp/ssrtest/caseview.v
+++
@@ -1,7 +0,0 @@
-(* (c) Copyright 2006-2016 Microsoft Corporation and Inria.                  *)
-(* Distributed under the terms of CeCILL-B.                                  *)
-Require Import mathcomp.ssreflect.ssreflect.
-
-
-Lemma test (A B : Prop) : A /\ B -> True.
-Proof. by case=> _ /id _. Qed.
--- mathcomp/ssrtest/move_after.v
+++
@@ -1,9 +0,0 @@
-(* (c) Copyright 2006-2016 Microsoft Corporation and Inria.                  *)
-(* Distributed under the terms of CeCILL-B.                                  *)
-Require Import mathcomp.ssreflect.ssreflect.
-
-
-Goal True -> True -> True.
-move=> H1 H2.
-move H1 after H2.
-Admitted.
