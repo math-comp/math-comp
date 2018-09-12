329cfb6e61e2a238f17e7becfeedb1a8e20f1471
Cyril Cohen <CohenCyril@users.noreply.github.com>
2018-09-12 14:00:10 +0200

    all2E is oneliner

--- mathcomp/ssreflect/seq.v
+++ mathcomp/ssreflect/seq.v
@@ -2796,9 +2796,8 @@ Fixpoint all2 s1 s2 :=
 
 Lemma all2E s1 s2: all2 s1 s2 =
   (size s1 == size s2) && (all [pred xy | p xy.1 xy.2] (zip s1 s2)).
-Proof.
-by elim: s1 s2 => [|x s1 ih] [|y s2] //=; rewrite ih andbCA.
-Qed.
+Proof. by elim: s1 s2 => [|x s1 ihs1] [|y s2] //=; rewrite ihs1 andbCA. Qed.
+
 End All2.
 
 Arguments all2 {T U} p !s1 !s2.
