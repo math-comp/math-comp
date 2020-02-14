1501c62c22ca9bf4b6f08f8d936962c2bb914efe
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-02-14 19:43:29 +0100

    attempt at building a safe space MC for "Search .... in MC"

--- mathcomp/ssreflect/mini_ssreflect.v
+++ mathcomp/ssreflect/mini_ssreflect.v
@@ -3,3 +3,16 @@ Require Export mini_ssrfun.
 Require Export mini_eqtype.
 Require Export mini_ssrnat.
 Require Export mini_seq.
+Require Export mini_div.
+Module MC.
+Module Bool.
+Include mini_ssrbool.
+End Bool.
+Module Nat.
+Include mini_ssrnat.
+Include mini_div.
+End Nat.
+Module List.
+Include mini_seq.
+End List.
+End MC.
