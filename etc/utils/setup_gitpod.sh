96d392fd70f91d2a5e71cb3d07dddb0c603280a7
Cyril Cohen <cyril.cohen@inria.fr>
2021-03-25 13:22:22 +0000

    fix gotpod

--- etc/utils/setup_gitpod.sh
+++ etc/utils/setup_gitpod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p .theia
-echo "{ \"coqtop.binPath\": \"$COQBIN\" }" > .theia/settings.json
+echo "{ \"coqtop.binPath\": \"$(dirname $(which coqtop))\" }" > .theia/settings.json
