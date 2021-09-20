6c66f38e8312624d866050284ec2591e85c96c11
Cyril Cohen <CohenCyril@users.noreply.github.com>
2021-09-20 11:13:55 +0200

    Update setup_gitpod.sh

--- etc/utils/setup_gitpod.sh
+++ etc/utils/setup_gitpod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir -p .theia
-echo "{ \"coqtop.binPath\": \"$(dirname $(which coqtop))\" }" > .theia/settings.json
+mkdir -p .vscode
+echo "{ \"coqtop.binPath\": \"$(dirname $(which coqtop))\" }" > .vscode/settings.json
