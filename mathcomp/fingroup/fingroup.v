9e7b40341421d2fe3612be348d17c4922f91c5f0
Cyril Cohen <cohen@crans.org>
2021-10-25 13:24:03 +0200

    fix hint rewrite locality

--- mathcomp/fingroup/fingroup.v
+++ mathcomp/fingroup/fingroup.v
@@ -632,6 +632,7 @@ Proof. exact/eqP/commgP/commuteV/commuteX. Qed.
 
 End GroupIdentities.
 
+#[global]
 Hint Rewrite mulg1 mul1g invg1 mulVg mulgV (@invgK) mulgK mulgKV
              invMg mulgA : gsimpl.
 
