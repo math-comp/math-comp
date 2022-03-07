43469aa2da65d3f72687ec3bd3ad7feac34f826b
affeldt-aist <33154536+affeldt-aist@users.noreply.github.com>
2022-03-07 21:54:33 +0900

    Update mathcomp/ssreflect/ssreflect.v
    Co-authored-by: Enrico Tassi <Enrico.Tassi@inria.fr>

--- mathcomp/ssreflect/ssreflect.v
+++ mathcomp/ssreflect/ssreflect.v
@@ -11,7 +11,6 @@ Global Set Bullet Behavior "None".
 (*          !! x == triggers pretyping to fill the holes of the term x. The   *)
 (*                  main use case is to trigger typeclass inference in the    *)
 (*                  body of a ssreflect have := !! body.                      *)
-(*                  Credits: Enrico Tassi.                                    *)
 (*                                                                            *)
 (*   Intro pattern ltac views:                                                *)
 (*   - calling rewrite from an intro pattern, use with parsimony              *)
@@ -20,34 +19,6 @@ Global Set Bullet Behavior "None".
 (******************************************************************************)
 
 Reserved Notation "!! x" (at level 100, only parsing).
-
-Module NonPropType.
-
-Structure call_of (condition : unit) (result : bool) := Call {callee : Type}.
-Definition maybeProp (T : Type) := tt.
-Definition call T := Call (maybeProp T) false T.
-
-Structure test_of (result : bool) := Test {condition :> unit}.
-Definition test_Prop (P : Prop) := Test true (maybeProp P).
-Definition test_negative := Test false tt.
-
-Structure type :=
-  Check {result : bool; test : test_of result; frame : call_of test result}.
-Definition check result test frame := @Check result test frame.
-
-Module Exports.
-Canonical call.
-Canonical test_Prop.
-Canonical test_negative.
-Canonical check.
-Notation nonPropType := type.
-Coercion callee : call_of >-> Sortclass.
-Coercion frame : type >-> call_of.
-Notation notProp T := (@check false test_negative (call T)).
-End Exports.
-
-End NonPropType.
-Export NonPropType.Exports.
 
 Module Deprecation.
 
