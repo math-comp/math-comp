bb903b7499cca2f49b9ac2865312ef4f2e2f72b1
Kazuhiko Sakaguchi <pi8027@gmail.com>
2024-08-06 16:10:54 +0200

    Cherry-pick from #1125

--- mathcomp/algebra/vector.v
+++ mathcomp/algebra/vector.v
@@ -678,10 +678,10 @@ Lemma dimv0 : \dim (0%VS : {vspace vT}) = 0.
 Proof. by rewrite /dimv vs2mx0 mxrank0. Qed.
 
 Lemma dimv_eq0 U :  (\dim U == 0) = (U == 0%VS).
-Proof. by rewrite /dimv /= mxrank_eq0 {2}/eq_op /= linear0 genmx0. Qed.
+Proof. by rewrite /dimv /= mxrank_eq0 [in RHS]/eq_op /= linear0 genmx0. Qed.
 
 Lemma dimvf : \dim {:vT} = dim vT.
-Proof.  by rewrite /dimv vs2mxF mxrank1. Qed.
+Proof. by rewrite /dimv vs2mxF mxrank1. Qed.
 
 Lemma dim_vline v : \dim <[v]> = (v != 0).
 Proof. by rewrite /dimv mxrank_gen rank_rV (can2_eq v2rK r2vK) linear0. Qed.
@@ -1083,7 +1083,7 @@ have lin_f: linear f.
   move=> k u v; rewrite scaler_sumr -big_split; apply: eq_bigr => i _.
   by rewrite /= scalerA -scalerDl linearP.
 pose flM := GRing.isLinear.Build _ _ _ _ f lin_f.
-pose fL : GRing.Linear.type _ _ := HB.pack f flM.
+pose fL : {linear _ -> _} := HB.pack f flM.
 exists fL => freeX eq_szX.
 apply/esym/(@eq_from_nth _ 0); rewrite ?size_map eq_szX // => i ltiX.
 rewrite (nth_map 0) //= /f (bigD1 (Ordinal ltiX)) //=.
@@ -1984,7 +1984,7 @@ have r2pK : cancel r2p p2r by move=> w; rewrite /p2r !r2vK hsubmxK.
 have p2rK : cancel p2r r2p by case=> u v; rewrite /r2p row_mxKl row_mxKr !v2rK.
 have r2p_lin: linear r2p by move=> a u v; congr (_ , _); rewrite /= !linearP.
 pose r2plM := GRing.isLinear.Build _ _ _ _ r2p r2p_lin.
-pose r2pL : GRing.Linear.type _ _ := HB.pack r2p r2plM.
+pose r2pL : {linear _ -> _} := HB.pack r2p r2plM.
 by exists p2r; [apply: (@can2_linear _ _ _ r2pL) | exists r2p].
 Qed.
 HB.instance Definition _ := Lmodule_hasFinDim.Build _ (vT1 * vT2)%type
@@ -2032,7 +2032,7 @@ Lemma vsolve_eqP (U : {vspace vT}) :
 Proof.
 have lhsZ: linear lhsf by move=> a u v; apply/ffunP=> i; rewrite !ffunE linearP.
 pose lhslM := GRing.isLinear.Build _ _ _ _ lhsf lhsZ.
-pose lhsL : GRing.Linear.type _ _ := HB.pack lhsf lhslM.
+pose lhsL : {linear _ -> _} := HB.pack lhsf lhslM.
 apply: (iffP memv_imgP) => [] [u Uu sol_u]; exists u => //.
   by move=> i; rewrite -[tnth rhs i]ffunE sol_u (lfunE lhsL) ffunE.
 by apply/ffunP=> i; rewrite (lfunE lhsL) !ffunE sol_u.
@@ -2051,7 +2051,7 @@ Lemma span_lfunP (U : seq uT) (phi psi : 'Hom(uT,vT)) :
 Proof.
 split=> eq_phi_psi u uU; first by rewrite eq_phi_psi ?memv_span.
 rewrite [u](@coord_span _ _ _ (in_tuple U))// !linear_sum/=.
-by apply: eq_bigr=> i _; rewrite !linearZ/= eq_phi_psi// ?mem_nth.
+by apply: eq_bigr=> i _; rewrite 2!linearZ/= eq_phi_psi// ?mem_nth.
 Qed.
 
 Lemma fullv_lfunP (U : seq uT) (phi psi : 'Hom(uT,vT)) : <<U>>%VS = fullv ->
