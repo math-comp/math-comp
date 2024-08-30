5cf3b31a23932c39f5b4d9a2c5efce20ff9e1bbf
thomas-lamiaux <thomas.lamiaux@ens-paris-saclay.fr>
2024-08-30 21:10:39 +0200

    update QuickChick

--- .nix/config.nix
+++ .nix/config.nix
@@ -68,6 +68,7 @@ with builtins; with (import <nixpkgs> {}).lib;
          coq-bits.override.version = "thomas-lamiaux:common_done";
          gaia.override.version = "thomas-lamiaux:common_done";
          coqeal.override.version = "thomas-lamiaux:common_done";
+         QuickChick.override.version = "thomas-lamiaux:common_done";
          # where
          # * <package> will typically be one of the strings above (without the quotes)
          #   or look at https://github.com/NixOS/nixpkgs/tree/master/pkgs/development/coq-modules
