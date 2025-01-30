1abe6e305dead2584fce17823d773e572470493d
Cyril Cohen <cohen@crans.org>
2025-01-30 17:40:23 +0100

    adding itv and numext

---
+++ algebra/num_theory/ssrnum.v
@@ -0,0 +1,24 @@
+From mathcomp Require Export orderedzmod.
+From mathcomp Require Export numdomain.
+From mathcomp Require Export numfield.
+From mathcomp Require Export itv.
+From mathcomp Require Export numext.
+
+Module Num.
+Export orderedzmod.Num.
+Export numdomain.Num.
+Export numfield.Num.
+
+Module Theory.
+Export orderedzmod.Num.Theory.
+Export numdomain.Num.Theory.
+Export numfield.Num.Theory.
+End Theory.
+
+Module Def.
+Export orderedzmod.Num.Def.
+Export numdomain.Num.Def.
+Export numfield.Num.Def.
+End Def.
+
+End Num.
