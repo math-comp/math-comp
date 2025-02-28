0a8703362dfa86558b266251d91b567b504c47e4
Pierre Roux <pierre.roux@onera.fr>
2025-02-28 08:59:11 +0100

    Drop support for Coq 8.19

--- ssreflect/ssrAC.v
+++ ssreflect/ssrAC.v
@@ -1,6 +1,7 @@
 From HB Require Import structures.
 From Corelib Require Import PosDef.
 (* use #[warning="-hiding-delimiting-key"] attribute once we require Coq 8.18 *)
+(* (the warning was completely removed in 9.0) *)
 Set Warnings "-hiding-delimiting-key".
 From mathcomp Require Import ssreflect ssrbool ssrfun ssrnat eqtype seq bigop.
 Set Warnings "hiding-delimiting-key".
