67649ebd2f53584a48c843f331bbd62b4e9571a5
Pierre Roux <pierre.roux@onera.fr>
2025-03-06 16:32:32 +0100

    Split ssrnum.v
    Nothing added nor removed, this commit just splits ssrnum.v
    into multiple files.

---
+++ algebra/num_theory/ssrnum.v
@@ -0,0 +1,22 @@
+From mathcomp Require Export orderedzmod.
+From mathcomp Require Export numdomain.
+From mathcomp Require Export numfield.
+
+Module Num.
+Export orderedzmod.Num.
+Export numdomain.Num.
+Export numfield.Num.
+
+Module Theory.
+Export orderedzmod.Num.Theory.
+Export numdomain.Num.Theory.
+Export numfield.Num.Theory.
+End Theory.
+
+Module Def.
+Export orderedzmod.Num.Def.
+Export numdomain.Num.Def.
+Export numfield.Num.Def.
+End Def.
+
+End Num.
