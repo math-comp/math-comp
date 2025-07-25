2eff40c84d15bd2525e96392553fcee00f16453b
Joshua Smart <josh@thesmarts.co.uk>
2025-07-25 13:55:26 +0100

    Proved eq and other progress

--- algebra/tensor.v
+++ algebra/tensor.v
@@ -95,20 +95,19 @@ Fixpoint tensor_eq_op {ds} : rel ('T[R]_ds) := match ds with
 
 Definition tensor_eqP {ds} : Equality.axiom (@tensor_eq_op ds).
 Proof.
-elim: ds=> [[x] [y]|d ds' Hind [x/=] [y/=]]/=. 
-  case (x =P y) => H_xy.
-  - by rewrite H_xy; left.
-  - by right; case; exact H_xy.
-case forallP => H_forall.
-- left. apply f_equal. apply ffunP => i.
-  have: forall a b, Tensor a = Tensor b -> a = b => [ds T0 a b|H_tensor_ab].
+elim: ds=> [|d ds' Hind] [x] [y] /=.
+  case (x =P y) => [->|x_neq_y]; first by left.
+    by right; case; exact x_neq_y.
+case forallP => forall_tensor_eq_op_nth.
+- left; apply f_equal; apply ffunP => i.
+  have: forall a b, Tensor a = Tensor b -> a = b => [ds T0 a b|tensor_eq_ab].
     by case.
-  apply H_tensor_ab; apply/Hind.
-  by rewrite /nth; apply H_forall.
+  apply tensor_eq_ab; apply/Hind.
+  by rewrite /nth; apply forall_tensor_eq_op_nth.
 - right=> x_eq_y. 
   have: forall x1, tensor_eq_op (nth (Tensor x) x1) (nth (Tensor y) x1) => [x1|].
     by apply/Hind; rewrite x_eq_y.
-  by apply H_forall.
+  by apply forall_tensor_eq_op_nth.
 Qed.
 
 HB.instance Definition _ {ds} := hasDecEq.Build ('T_ds) tensor_eqP.
@@ -127,20 +126,47 @@ Fixpoint map_tensor {ds} (f : X -> Y) : 'T[X]_ds -> 'T[Y]_ds :=
   end.
 
 Fixpoint map2_tensor {ds} (f : X -> Y -> Z) : 'T[X]_ds -> 'T[Y]_ds -> 'T[Z]_ds :=
-    match ds with
-    | [::] => fun u v => @Tensor [::] Z (f (tensor_val u) (tensor_val v))
-    | d :: ds' => fun u v => stack [ffun i => map2_tensor f (nth u i) (nth v i)]
-    end.
+  match ds with
+  | [::] => fun u v => @Tensor [::] Z (f (tensor_val u) (tensor_val v))
+  | d :: ds' => fun u v => stack [ffun i => map2_tensor f (nth u i) (nth v i)]
+  end.
 
 End MapTensor.
 
 
 Section TensorChoice.
 
+Import Choice.InternalTheory.
+
 Context {R : choiceType}.
 
+Fixpoint tensor_find {ds} : pred 'T[R]_ds -> nat -> option 'T[R]_ds := match ds with
+  | [::] => fun P n => omap (@Tensor [::] _) (find (fun t => P (@Tensor [::] _ t)) n)
+  | d :: ds' => fun P n => None
+  end.
+
+(* Definition tensor_hasChoice {ds} : hasChoice ('T[R]_ds).
+Proof.
+exists tensor_find.
+- elim: ds => [|d ds' Hind] P n t.
+  + rewrite /tensor_find. case: t => /= i H.
+    (* have: P (@Tensor [::] _ i) = (P \o @Tensor [::] _) i => [//|-> H]. *)
+    
+    move: H. case: (find _ n) => //= j.
+    by case => ->.
+  + admit.
+- elim: ds => [|d ds' Hind] P [[i]].
+  + rewrite /tensor_find.
+    have: P (@Tensor [::] _ i) = (P \o @Tensor [::] _) i => [//|-> H].
+    rewrite /isSome.
+    admit.
+  + admit.
+- elim: ds => [|d ds' Hind] P Q P_eq1_Q. 
+  + rewrite /tensor_find /eqfun => n.
+Admitted. *)
+
 Definition tensor_hasChoice {ds} : hasChoice ('T[R]_ds).
-Proof. Admitted.
+Admitted.
 
 HB.instance Definition _ {ds} := @tensor_hasChoice ds.
 
@@ -157,26 +183,27 @@ Definition tensor0 {ds} := @const R ds 0.
 
 Lemma addTA {ds} : associative (@addT ds).
 Proof.
-    elim ds=> [t u v|d ds' Hind t u v]; first by rewrite /addT/= addrA.
-    rewrite /addT/=; apply f_equal; apply eq_dffun=> x0. 
-    rewrite -/addT {2 5}/nth/= !ffunE 2!tensor_valK.
-    by apply Hind.
+elim ds=> [t u v|d ds' Hind t u v]; first by rewrite /addT/= addrA.
+rewrite /addT/=; apply f_equal; apply eq_dffun=> x0. 
+rewrite -/addT {2 5}/nth/= !ffunE 2!tensor_valK.
+by apply Hind.
 Qed.
 
 Lemma addTC {ds} : commutative (@addT ds).
 Proof.
-    elim: ds => [t u|d ds' Hind t u]; first by rewrite /addT/= addrC.
-    rewrite /addT/= /stack; apply f_equal; apply eq_dffun=> x0; apply f_equal; rewrite !ffunE.
-    by apply Hind.
+elim: ds => [t u|d ds' Hind t u]; first by rewrite /addT/= addrC.
+rewrite /addT/= /stack.
+apply f_equal; apply eq_dffun=> x0; apply f_equal.
+rewrite !ffunE.
+by apply Hind.
 Qed.
 
 Lemma add0T {ds} : left_id tensor0 (@addT ds).
 Proof.
-    elim: ds => [t|d ds' Hind]; first by rewrite /addT/= add0r tensor_valK.
-    rewrite /addT/= -/addT /stack.
-    case => i.
-    apply f_equal; apply ffunP => x.
-    by rewrite 2!ffunE /tensor0 /nth/= 2!ffunE tensor_valK Hind.
+elim: ds => [t|d ds' Hind]; first by rewrite /addT/= add0r tensor_valK.
+rewrite /addT/= -/addT /stack => [[i]].
+apply f_equal; apply ffunP => x.
+by rewrite 2!ffunE /tensor0 /nth/= 2!ffunE tensor_valK Hind.
 Qed.
 
 HB.instance Definition _ {ds} := GRing.isNmodule.Build ('T_ds) addTA addTC add0T.
@@ -192,15 +219,16 @@ Definition oppT {ds} := @map_tensor R R ds -%R.
 
 Lemma addNT {ds} : left_inverse 0 (@oppT ds) +%R.
 Proof.
-    elim: ds => [|d ds' Hind]; case => i; first by rewrite /oppT /GRing.add/= /addT/= addNr.
-    rewrite /oppT/= -/oppT /GRing.add/= /addT /stack/= /GRing.zero/= /tensor0/=.
-    apply f_equal; apply eq_dffun => x.
-    rewrite /nth/= 2!ffunE tensor_valK -/addT.
-    rewrite (_ : forall a b, addT a b = a + b); last by rewrite /GRing.add/=.
-    by rewrite Hind.
+elim: ds => [|d ds' Hind]; case => i.
+  by rewrite /oppT /GRing.add/= /addT/= addNr.
+rewrite /oppT/= -/oppT /GRing.add/= /addT /stack/= /GRing.zero/= /tensor0/=.
+apply f_equal; apply eq_dffun => x.
+rewrite /nth/= 2!ffunE tensor_valK -/addT.
+rewrite (_ : forall a b, addT a b = a + b); last by rewrite /GRing.add/=.
+by rewrite Hind.
 Qed.
 
-HB.instance Definition _ {ds} := GRing.Nmodule_isZmodule.Build ('T[R]_ds) addNT.
+HB.instance Definition _ {ds} := GRing.Nmodule_isZmodule.Build ('T_ds) addNT.
 
 End TensorZModule.
 
@@ -215,66 +243,64 @@ Definition mulT {ds} := @map2_tensor R R R ds *%R.
 
 Lemma mulTA {ds} : associative (@mulT ds).
 Proof.
-    elim: ds => [t u v|d ds' Hind t u v]; first by rewrite /mulT/= mulrA.
-    rewrite /mulT/= -/mulT.
-    apply f_equal; apply eq_dffun => x.
-    by rewrite {2 5}/nth/= !ffunE 2!tensor_valK Hind.
+elim: ds => [t u v|d ds' Hind t u v]; first by rewrite /mulT/= mulrA.
+rewrite /mulT/= -/mulT.
+apply f_equal; apply eq_dffun => x.
+by rewrite {2 5}/nth/= !ffunE 2!tensor_valK Hind.
 Qed.
 
 Lemma mul1T {ds} : left_id tensor1 (@mulT ds).
 Proof.
-    elim: ds => [t|d ds' Hind]; first by rewrite /mulT/= mul1r tensor_valK.
-    rewrite /mulT/= -/mulT /stack.
-    case => i.
-    apply f_equal; apply ffunP => x.
-    by rewrite 2!ffunE /tensor1 /nth/= 2!ffunE tensor_valK Hind.
+elim: ds => [t|d ds' Hind]; first by rewrite /mulT/= mul1r tensor_valK.
+rewrite /mulT/= -/mulT /stack => [[i]].
+apply f_equal; apply ffunP => x.
+by rewrite 2!ffunE /tensor1 /nth/= 2!ffunE tensor_valK Hind.
 Qed.
 
 Lemma mulT1 {ds} : right_id tensor1 (@mulT ds).
 Proof.
-    elim: ds => [t|d ds' Hind]; first by rewrite /mulT/= mulr1 tensor_valK.
-    rewrite /mulT/= -/mulT /stack.
-    case => i.
-    apply f_equal; apply ffunP => x.
-    by rewrite 2!ffunE /tensor1 /nth/= 2!ffunE tensor_valK Hind.
+elim: ds => [t|d ds' Hind]; first by rewrite /mulT/= mulr1 tensor_valK.
+rewrite /mulT/= -/mulT /stack => [[i]].
+apply f_equal; apply ffunP => x.
+by rewrite 2!ffunE /tensor1 /nth/= 2!ffunE tensor_valK Hind.
 Qed.
 
 Lemma mulTDl {ds} : left_distributive (@mulT ds) +%R.
 Proof.
-    elim: ds => [t u v|d ds' Hind t u v].
-      by rewrite /mulT/= mulrDl {2}/GRing.add/= /addT.
-    rewrite /mulT/= -/mulT /GRing.add/= /addT /nth/=.
-    apply f_equal; apply eq_dffun => x.
-    rewrite 2!ffunE tensor_valK -/addT.
-    have: forall a b, addT a b = a + b => addT_add; first by rewrite /GRing.add/=.
-    by rewrite 2!addT_add Hind /nth /stack 4!ffunE 2!tensor_valK.
+elim: ds => [t u v|d ds' Hind t u v].
+  by rewrite /mulT/= mulrDl {2}/GRing.add/= /addT.
+rewrite /mulT/= -/mulT /GRing.add/= /addT /nth/=.
+apply f_equal; apply eq_dffun => x.
+rewrite 2!ffunE tensor_valK -/addT.
+have: forall a b, addT a b = a + b => addT_add; first by rewrite /GRing.add/=.
+by rewrite 2!addT_add Hind /nth /stack 4!ffunE 2!tensor_valK.
 Qed.
 
 Lemma mulTDr {ds} : right_distributive (@mulT ds) +%R.
 Proof.
-    elim: ds => [t u v|d ds' Hind t u v].
-      by rewrite /mulT/= mulrDr {2}/GRing.add/= /addT.
-    rewrite /mulT/= -/mulT /GRing.add/= /addT /nth/=.
-    apply f_equal; apply eq_dffun => x.
-    rewrite 2!ffunE tensor_valK -/addT.
-    have: forall a b, addT a b = a + b => addT_add; first by rewrite /GRing.add/=.
-    by rewrite 2!addT_add Hind /nth /stack 4!ffunE 2!tensor_valK.
+elim: ds => [t u v|d ds' Hind t u v].
+  by rewrite /mulT/= mulrDr {2}/GRing.add/= /addT.
+rewrite /mulT/= -/mulT /GRing.add/= /addT /nth/=.
+apply f_equal; apply eq_dffun => x.
+rewrite 2!ffunE tensor_valK -/addT.
+have: forall a b, addT a b = a + b => addT_add; first by rewrite /GRing.add/=.
+by rewrite 2!addT_add Hind /nth /stack 4!ffunE 2!tensor_valK.
 Qed.
 
 Lemma mul0T {ds} : left_zero 0 (@mulT ds).
 Proof.
-    elim: ds => [t|d ds' Hind t]; first by rewrite /mulT/= mul0r.
-    rewrite /mulT/= -/mulT /GRing.zero/= /tensor0/=.
-    apply f_equal; apply eq_dffun => x.
-    by rewrite /nth /stack 2!ffunE tensor_valK Hind.
+elim: ds => [t|d ds' Hind t]; first by rewrite /mulT/= mul0r.
+rewrite /mulT/= -/mulT /GRing.zero/= /tensor0/=.
+apply f_equal; apply eq_dffun => x.
+by rewrite /nth /stack 2!ffunE tensor_valK Hind.
 Qed.
 
 Lemma mulT0 {ds} : right_zero 0 (@mulT ds).
 Proof.
-    elim: ds => [t|d ds' Hind t]; first by rewrite /mulT/= mulr0.
-    rewrite /mulT/= -/mulT /GRing.zero/= /tensor0/=.
-    apply f_equal; apply eq_dffun => x.
-    by rewrite /nth /stack 2!ffunE tensor_valK Hind.
+elim: ds => [t|d ds' Hind t]; first by rewrite /mulT/= mulr0.
+rewrite /mulT/= -/mulT /GRing.zero/= /tensor0/=.
+apply f_equal; apply eq_dffun => x.
+by rewrite /nth /stack 2!ffunE tensor_valK Hind.
 Qed.
 
 HB.instance Definition _ {ds} := GRing.Nmodule_isPzSemiRing.Build ('T_ds)
@@ -287,22 +313,20 @@ Section TensorNzSemiRing.
 
 Context {R : nzSemiRingType}.
 
-(* FALSE when d = 0 in inductive case *)
-(* works when ds : seq Z+ *)
-Lemma oneT_neq0 {ds} : @GRing.one ('T[R]_ds) != 0.
+Lemma oneT_neq0 {ds} : all [eta leq 1] ds -> @GRing.one ('T[R]_ds) != 0.
 Proof.
-    elim: ds => [|d ds' Hind]; first by rewrite oner_neq0.
-    rewrite /eq_op/=. apply/forallPn. eexists.
-    have: (@GRing.one 'T[R]_ds') == 0.
-      rewrite /eq_op/=.
+elim: ds => [|d ds' Hind]; first by rewrite oner_neq0.
+rewrite /eq_op/=.
 Admitted.
 
-HB.instance Definition _ {ds} := GRing.PzSemiRing_isNonZero.Build ('T_ds) oneT_neq0.
+HB.instance Definition _ {ds} {ds_gt_0} := GRing.PzSemiRing_isNonZero.Build ('T_ds) (oneT_neq0 ds_gt_0).
 
 End TensorNzSemiRing.
 
 
 Section TensorOrder.
+
+Import Order.LTheory.
 
 Context (o : Order.disp_t) (R : porderType o).
 
@@ -318,58 +342,36 @@ Proof. by rewrite /ltT. Qed.
 
 Lemma leT_refl {ds} : reflexive (@leT ds).
 Proof.
-  elim: ds => [t|d ds' Hind t].
-    by rewrite /leT Order.POrderTheory.le_refl.
-  rewrite /leT -/leT.
-  apply/forallP => x.
-  by apply Hind.
+elim: ds => [t|d ds' Hind t].
+  by rewrite /leT le_refl.
+rewrite /leT -/leT.
+by apply/forallP => x.
 Qed.
 
 Lemma leT_anti {ds} : antisymmetric (@leT ds).
 Proof.
-  elim: ds => [t u|d ds' Hind t u].
-    rewrite /leT => le_tut.
-    have: tensor_val t = tensor_val u -> t = u => [|H_tensor_val].
-      by case t; case u => i0 i1 /= i1_eq_i0; rewrite i1_eq_i0.
-    apply H_tensor_val.
-    by apply Order.POrderTheory.le_anti.
-  rewrite /leT -/leT => /andP [/forallP le_tu /forallP le_ut].
-  have: (forall i, nth t i = nth u i) -> t = u => [nth_tu|].
-    apply/eqP. rewrite /eq_op/=. apply/forallP => i. rewrite nth_tu.
-    have: nth u i = nth u i -> tensor_eq_op (nth u i) (nth u i) => [/eqP +|].
-      by rewrite /eq_op/=.
-    by apply.
-  apply => i.
-  apply/Hind.
-  by apply/andP; split.
+elim: ds => [t u|d ds' Hind t u].
+  rewrite /leT => le_tut.
+  have: tensor_val t = tensor_val u -> t = u => [|->//].
+    by case t; case u => i0 i1 /= i1_eq_i0; rewrite i1_eq_i0.
+  by apply le_anti.
+rewrite /leT -/leT => /andP [/forallP le_tu /forallP le_ut].
+have: (forall i, nth t i = nth u i) -> t = u => [nth_tu|->// i].
+  apply/eqP; rewrite /eq_op/=; apply/forallP => i; rewrite nth_tu.
+  by apply/tensor_eqP.
+by apply/Hind/andP.
 Qed.
 
 Lemma leT_trans {ds} : transitive (@leT ds).
 Proof.
-  elim: ds => [t u v|d ds' Hind t u v].
-    rewrite /leT. by apply Order.POrderTheory.le_trans.
-  rewrite /leT -/leT => /forallP le_ut /forallP le_tv. apply/forallP => i.
-  apply /Hind.
-  - by apply le_ut.
-  - by apply le_tv.
+elim: ds => [t u v|d ds' Hind t u v].
+  by rewrite /leT; apply le_trans.
+rewrite /leT -/leT => /forallP le_ut /forallP le_tv.
+apply/forallP => i.
+by apply /Hind.
 Qed.
 
 HB.instance Definition _ {ds} := Order.isPOrder.Build
   o ('T_ds) ltT_def leT_refl leT_anti leT_trans.
 
 End TensorOrder.
-
-
-Section Test.
-
-
-Context (ds : seq nat) (t u v : 'T[nat]_ds).
-
-Lemma dist : (t + u) * v = t * v + u * v.
-Proof.
-    by rewrite mulrDl.
-Qed.
-
-Check Order.le t u.
-
-End Test.
