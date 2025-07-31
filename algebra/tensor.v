5bad1f8131f8eabbd42a9488caaf3b3771f0d2b8
Joshua Smart <josh@thesmarts.co.uk>
2025-07-31 14:32:23 +0200

    Added additional Nmod, Zmod, and (Com)Pz(Semi)Ring instances

--- algebra/tensor.v
+++ algebra/tensor.v
@@ -37,72 +37,158 @@ Section TensorDef.
 
 Context (us ds : seq nat) (K : Type).
 
-Variant tensor : predArgType := Tensor of 'M[K]_(\prod_(u <- us) u, \prod_(d <- ds) d).
+Variant tensor : predArgType :=
+  Tensor of 'M[K]_(\prod_(u <- us) u, \prod_(d <- ds) d).
 
-Definition T_val T := let: Tensor g := T in g.
+Definition t_val T := let: Tensor g := T in g.
 
-HB.instance Definition _ := [isNew for T_val].
+HB.instance Definition _ := [isNew for t_val].
+
+Coercion t_val : tensor >-> matrix.
 
 End TensorDef.
+
 
 Notation "''T[' R ]_ ( us , ds )" := (tensor us ds R) (only parsing).
 Notation "''T_' ( us , ds )" := 'T[_]_(us, ds).
 
+
 Section SubtypeInstances.
 
-Context (us ds : seq nat).
+Import Algebra.
 
-HB.instance Definition _ (R : eqType) := [Equality of 'T[R]_(us, ds) by <:].
-HB.instance Definition _ (R : choiceType) := [Choice of 'T[R]_(us, ds) by <:].
-HB.instance Definition _ (R : countType) := [Countable of 'T[R]_(us, ds) by <:].
-HB.instance Definition _ (R : finType) := [Finite of 'T[R]_(us, ds) by <:].
+Context (us ds : seq nat).
+Local Notation "''T[' R ]" := 'T[R]_(us, ds).
+
+HB.instance Definition _ (R : eqType) := [Equality of 'T[R] by <:].
+HB.instance Definition _ (R : choiceType) := [Choice of 'T[R] by <:].
+HB.instance Definition _ (R : countType) := [Countable of 'T[R] by <:].
+HB.instance Definition _ (R : finType) := [Finite of 'T[R] by <:].
+
+Lemma nmod_closed {m n} (R : nmodType) : @nmod_closed 'M[R]_(n, m) predT.
+Proof. by []. Qed.
+HB.instance Definition _ (R : nmodType) := SubChoice_isSubNmodule.Build
+  _ _ 'T[R] (nmod_closed R).
+
+Lemma zmod_closed {m n} (R : zmodType) : @zmod_closed 'M[R]_(n, m) predT.
+Proof. by []. Qed.
+HB.instance Definition _ (R : zmodType) := SubChoice_isSubZmodule.Build
+  _ _ 'T[R] (zmod_closed R).
+
+HB.about tensor.
 
 End SubtypeInstances.
 
+
+Definition const_t {R us ds} (v : R) : 'T[R]_(us, ds) :=
+  Tensor (const_mx v).
+
 Section IndexTensor.
 
-Context (us ds : seq nat) (R : Type).
+Context (R : Type) (u d : nat) (us ds : seq nat).
 
-Definition indexT (t : 'T[R]_(us, ds)) (ui : _) (di : _) : R := T_val t ui di.
-
-End IndexTensor.
-
-Section ContravariantTensor.
-
-Context (u' : nat) (us ds : seq nat) (R : Type).
-Local Notation u := u'.+1 (only parsing).
-
-
-
-Lemma ord_i_j (i : 'I_u) (j : 'I_\prod_(e <- us) e) : j * u + i < \prod_(e <- u :: us) e.
+Lemma ltn_ord_index {x} {xs} (i : 'I_x) (j : 'I_\prod_(e <- xs) e) 
+  : j * x + i < \prod_(e <- x :: xs) e.
 Proof.
-  case: i => i i_ord /=.
-  case: j => j j_ord /=.
-  rewrite big_cons. 
-  set e := \prod_(e <- us) e in j_ord *.
-  set u := u'.+1 in i_ord *.
-  by rewrite -ltn_subRL mulnC -mulnBl (ltn_leq_trans i_ord)// leq_pmull// subn_gt0.
+case: i => i i_ord /=; case: j => j j_ord /=.
+rewrite big_cons -ltn_subRL mulnC -mulnBl (ltn_leq_trans i_ord)// leq_pmull//.
+by rewrite subn_gt0.
 Qed.
 
 Definition upper_index (t : 'T[R]_(u :: us, ds)) (i : 'I_u) : 'T[R]_(us, ds) :=
-  Tensor (rowsub (fun j => Ordinal (ord_i_j i j)) (T_val t)).
+  Tensor (rowsub (fun j => Ordinal (ltn_ord_index i j)) t).
 
-End ContravariantTensor.
+Definition lower_index (t : 'T[R]_(us, d :: ds)) (i : 'I_d) : 'T[R]_(us, ds) :=
+  Tensor (colsub (fun j => Ordinal (ltn_ord_index i j)) t).
 
-Section CovariantTensor.
+End IndexTensor.
 
-Context (d : nat) (us ds : seq nat) (R : Type).
 
-Definition lower_index (t : 'T[R]_(us, d :: ds)) (i : 'I_d) : 'T[R]_(us, ds).
+Section NilTensor.
+
+Context (R : Type).
+
+Lemma ord_nil : 0 < \prod_(x <- [::]) x.
+Proof. by rewrite big_nil. Qed.
+
+Definition tensor_nilE (t : 'T[R]_([::], [::])) : R := 
+  t (Ordinal ord_nil) (Ordinal ord_nil).
+
+End NilTensor.
+
+
+Section TensorRing.
+
+Import GRing.Theory.
+
+Context (us ds : seq nat).
+Local Notation "''T[' R ]" := 'T[R]_(us, ds).
+
+Section TensorPzSemiRing.
+
+Context {R : pzSemiRingType}.
+
+Definition tensor1 := @const_t _ us ds (GRing.one R).
+
+Definition mult (t u : 'T[R]_(us, ds)) := @Tensor us ds R (map2_mx *%R t u).
+
+Lemma multA : associative mult.
+Proof. by move=> x y z; rewrite /mult map2_mxA. Qed.
+Lemma mul1t : left_id tensor1 mult.
+Proof. by move=> [x]; rewrite /mult map2_1mx. Qed.
+Lemma mult1 : right_id tensor1 mult.
+Proof. by move=> [x]; rewrite /mult map2_mx1. Qed.
+Lemma multDl : left_distributive mult +%R.
+Proof. by move=> x y z; rewrite /mult map2_mxDl. Qed.
+Lemma multDr : right_distributive mult +%R.
+Proof. by move=> x y z; rewrite /mult map2_mxDr. Qed.
+Lemma mul0t : left_zero 0%R mult.
+Proof. by move=> x; rewrite /mult map2_0mx. Qed.
+Lemma mult0 : right_zero 0%R mult.
+Proof. by move=> x; rewrite /mult map2_mx0. Qed.
+
+HB.instance Definition _ := GRing.Nmodule_isPzSemiRing.Build
+  'T[R] multA mul1t mult1 multDl multDr mul0t mult0.
+
+End TensorPzSemiRing.
+
+Lemma multC {R : comPzSemiRingType} : commutative (@mult R).
+Proof. by move=> x y; rewrite /mult map2_mxC. Qed.
+
+HB.instance Definition _ {R : comPzSemiRingType} := 
+  GRing.Nmodule_isComPzSemiRing.Build 
+  'T[R] multA multC mul1t multDl mul0t.
+
+HB.instance Definition _ {R : pzRingType} := GRing.Zmodule_isPzRing.Build
+  'T[R] multA mul1t mult1 multDl multDr.
+
+HB.instance Definition _ {R : comPzRingType} := 
+  GRing.PzRing_hasCommutativeMul.Build 'T[R] multC.
+
+Lemma onet_neq0 {R : nzSemiRingType} : (1%R : 'T[R]) != 0%R.
+Proof. 
+  rewrite /GRing.one /GRing.zero /= /tensor1 /const_t /Sub /=.
+  apply/eqP.
 Admitted.
 
+HB.instance Definition _ {R : nzSemiRingType} := 
+  GRing.PzSemiRing_isNonZero.Build
+  'T[R] onet_neq0.
 
-End CovariantTensor.
+HB.instance Definition _ {R : comNzSemiRingType} := 
+  GRing.Nmodule_isComNzSemiRing.Build
+  'T[R] multA multC mul1t multDl mul0t onet_neq0.
+
+End TensorRing.
+
 
 Section Test.
 
-Context (t v : 'T[bool]_([:: 2; 3], [:: 2])) (u : 'T[unit]_([::], [::])).
+Open Scope ring_scope.
 
-Check t == v.
+Context (R : comPzRingType) (t u : 'T[R]_([:: 2; 3], [:: 2])).
+
+Lemma comt : t * u = u * t.
+Proof. by rewrite GRing.mulrC. Qed.
 
 End Test.
