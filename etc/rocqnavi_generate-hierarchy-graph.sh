5209666a40aa48403b8ff2bd4a6357aec4e79717
Alessandro Bruni <brun@itu.dk>
2026-02-25 14:32:58 +0100

    fix dependency on coq shim

--- etc/rocqnavi_generate-hierarchy-graph.sh
+++ etc/rocqnavi_generate-hierarchy-graph.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-DST=$1
-@@#!/bin/bash
-@@DST=$1
-coqtop -Q . mathcomp <<EOF
+DST=$2
+$1 top -Q . mathcomp <<EOF
 From HB Require Import structures.
 From mathcomp Require Import all.
 HB.graph "$DST".
