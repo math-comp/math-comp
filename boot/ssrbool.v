0e635cb241738c14b461c1fffde575312f240961
Holger Thies <info@holgerthies.com>
2026-04-01 14:58:33 +0900

    added projections for and4 and and5

--- boot/ssrbool.v
+++ boot/ssrbool.v
@@ -48,3 +48,29 @@ Definition and3proj2 (P1 P2 P3 : Prop) (a : [/\ P1, P2 & P3]) :=
 Definition and3proj3 (P1 P2 P3 : Prop) (a : [/\ P1, P2 & P3]) :=
   let: And3 p1 p2 p3 := a in p3.
 
+Definition and4proj1 (P1 P2 P3 P4 : Prop) (a : [/\ P1, P2, P3 & P4]) :=
+  let: And4 p1 p2 p3 p4 := a in p1.
+
+Definition and4proj2 (P1 P2 P3 P4 : Prop) (a : [/\ P1, P2, P3 & P4]) :=
+  let: And4 p1 p2 p3 p4 := a in p2.
+
+Definition and4proj3 (P1 P2 P3 P4 : Prop) (a : [/\ P1, P2, P3 & P4]) :=
+  let: And4 p1 p2 p3 p4 := a in p3.
+
+Definition and4proj4 (P1 P2 P3 P4 : Prop) (a : [/\ P1, P2, P3 & P4]) :=
+  let: And4 p1 p2 p3 p4 := a in p4.
+
+Definition and5proj1 (P1 P2 P3 P4 P5 : Prop) (a : [/\ P1, P2, P3, P4 & P5]) :=
+  let: And5 p1 p2 p3 p4 p5 := a in p1.
+
+Definition and5proj2 (P1 P2 P3 P4 P5 : Prop) (a : [/\ P1, P2, P3, P4 & P5]) :=
+  let: And5 p1 p2 p3 p4 p5 := a in p2.
+
+Definition and5proj3 (P1 P2 P3 P4 P5 : Prop) (a : [/\ P1, P2, P3, P4 & P5]) :=
+  let: And5 p1 p2 p3 p4 p5 := a in p3.
+
+Definition and5proj4 (P1 P2 P3 P4 P5 : Prop) (a : [/\ P1, P2, P3, P4 & P5]) :=
+  let: And5 p1 p2 p3 p4 p5 := a in p4.
+
+Definition and5proj5 (P1 P2 P3 P4 P5 : Prop) (a : [/\ P1, P2, P3, P4 & P5]) :=
+  let: And5 p1 p2 p3 p4 p5 := a in p5.
